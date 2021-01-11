ca144d95d8b1f480dcce28961c65d1a33c0bdd39
Goren Barak <gorenemail@gmail.com>
2021-01-11 09:28:40 -0500

    added wait

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ for line in code.splitlines():
         answer = input(line[4:])
     elif words[0] == 'write':
         print((line[6:]).replace('{answer}',answer))
+    elif words[0] == 'wait':
+        import time
+        time.sleep(int(words[1]))
